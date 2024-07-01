1f19019c63e24b82429e42741b1ceed864553873
Loriana-Boixadera <lorianaboixadera@gmail.com>
2024-07-01 09:18:00 -0300

    Scrape Argenprop only

--- scraprealestate/scraprealestate/spiders/realestatespider.py
+++ scraprealestate/scraprealestate/spiders/realestatespider.py
@@ -1,14 +1,15 @@
+import re
 import scrapy
 from random import sample
 from scrapy.http import HtmlResponse
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
 
 
 # How many pages are for each domanins? (get into ramdonly)
-# There are 2 different real estates:
-#       - Argenprop --> por pag muestra 20 / (250) 14 paginas a consultar
-#       - Zonaprop --> por pag muestra 20 / (250) 14 paginas a consultar
+# There is real estate agency:
+#       - Argenprop --> por pag muestra 20 / (500) 30 paginas a consultar --> Scrapy
 
 AMENITIES = [
     "piscina",
@@ -24,14 +25,41 @@ AMENITIES = [
     "terraza",
     "espaciodeplaza",
     "juegos",
+    "juegosparachicos",
     "salóndefiestas"
 ]
+PATH_TO_FILE = "C:/Users/loria/Documents/github-projects/work-data_extracting/nomenclador_de_calles/Nomenclador de Calles.csv"
+df_calles = pd.read_csv(
+                PATH_TO_FILE,
+                sep=",",
+                usecols=["NOMBRE","TIPO","NOMBRE_ABREV"]
+            )
+
+calles=[
+    "DORREGO 34",
+    "ARBORIA 05 01  EDIFICIO 1",
+    "REPUBLICA SIRIA 100, PISO 2º",
+    "MAIPÚ 2200, PISO 10",
+    "VERA MÚJICA 860",
+    "MAUI - AV ESTANISLAO LOPEZ 2600. 44 - 1A",
+    "JUAN MANUEL DE ROSAS 1500, PISO 4",
+    "PASCO AL 1800 2 DORMITORIOS",
+    "PTE. ROCA 1500",
+    "AV. PELLEGRINI  4043 | MONOAMBIENTE",
+    "MAUI. AV. ESTANISLAO LOPEZ 2671. S31A2",
+    "QUINQUELA PLAZA",
+    "BV. AVELLANEDA 1080 BIS - TORRE BRISA - VISTA RIO",
+    "9 DE JULIO  357 | 1 DORMITORIO (2-03)",
+    "1 DE MAYO  AL 2300"
+]
+
+
 
 class ArgenpropSpider(scrapy.Spider):
     name = "argenprop_spider"
     allowed_domains = ["www.argenprop.com"]
     start_urls = ["https://www.argenprop.com/departamentos/venta/rosario-santa-fe"]
-    pages_to_scrape = 13
+    pages_to_scrape = 2 # 30
 
     def parse(self, response):
         estates = response.css('div.listing__item')
@@ -41,10 +69,8 @@ class ArgenpropSpider(scrapy.Spider):
         for estate in estates:
             price = estate.css('.card__monetary-values .card__price ::text').getall()
 
-            yield {
-                "price":        price[1] + str.strip(price[2]),
-                "url":          estate.css('a').attrib['href']
-            }
+            if "Consultar precio" in price[1]:
+                continue
 
             relative_estate = estate.css('a').attrib['href']
 
@@ -59,64 +85,110 @@ class ArgenpropSpider(scrapy.Spider):
         #     yield response.follow(next_page_url, callback=self.parse)
 
     def clean_sections(self, section_resp):
-        properties = []
+        properties = {}
         for item in section_resp:
-            properties.append(
-                {
-                    item.css("li p::text").get().replace(" ", "").split("\n")[1].replace(":",""): item.css("li p strong::text").get().replace(" ", "").split("\n")[1]
-                }
-            )
+            try:
+                title = item.css("li p::text").get().replace(" ", "").split("\n")[1].replace(":","")
+            except:
+                title = None
+            try:
+                charac = item.css("li p strong::text").get().replace(" ", "").split("\n")[1]
+            except:
+                charac = None
+            properties[title] = charac
         return properties
+
+    def replace_accent(self, address_to_parse):
+        dict_letter = {
+            "Á" : "A",
+            "É" : "E",
+            "Í" : "I",
+            "Ó" : "O",
+            "Ú" : "U"
+        }
+        for elem in ["Á","É","Í","Ó","Ú"]:
+            if elem in address_to_parse:
+                address_to_parse.replace(elem, dict_letter.get(elem))
+            else:
+                continue
+        return address_to_parse
+
+    def parse_street(self, street):
+        street = re.split(r"\s\d", street)[0]
+        if "MAUI" in street:
+            street = street.split("MAUI")[1]
+            if " - " in street:
+                street = street.split(" - ")[1]
+        if " AL" in street:
+            street = street.split(" AL")[0]
+        if "AV" in street:
+            try:
+                street = re.split(r"AV[.|\s]", street)[1].strip()
+            except:
+                pass
+        if "BV" in street:
+            try:
+                street = re.split(r"BV[.|\s]", street)[1].strip()
+            except:
+                pass
+        if "1" in street:
+            street = street.replace("1", "PRIMERO")
+        return street
+
+    def get_street_type(self, address):
+        calle = self.replace_accent(self.parse_street(address).upper())
+
+        df = df_calles[
+                df_calles["NOMBRE"].str.contains(calle) | df_calles["NOMBRE_ABREV"].str.contains(calle)
+        ]
+        if df.empty:
+            calle = calle.split(" ")
+            if calle.__len__() > 1:
+                df = df_calles[
+                    (df_calles["NOMBRE"].str.contains(calle[-2]) & df_calles["NOMBRE"].str.contains(calle[-1])) |
+                    (df_calles["NOMBRE_ABREV"].str.contains(calle[-2]) & df_calles["NOMBRE_ABREV"].str.contains(calle[-1]))
+                ]
+        if not df.empty:
+            if df.__len__() > 1:
+                df = df[df["NOMBRE_ABREV"] == calle]
+                if not df.empty:
+                    street_type = "1" if df["TIPO"].item().strip() == "C" else "0"
+                else: street_type = None
+            else:
+                street_type = "1" if df["TIPO"].item().strip() == "C" else "0"
+        else: street_type = "NOT STREET FOUND"
+        return street_type
+
 
     def parse_real_estate_page(self, response):
         address = response.css("div.location-container h2::text").get()
-        zone_location = response.css("div.location-container p::text").get()
-        section_caracteristicas = response.xpath("//ul[@id='section-caracteristicas']/li")
-        section_superficies = response.xpath("//ul[@id='section-superficie']/li")
-        section_inst_edif = response.xpath("//ul[@id='section-instalaciones-edificio']/li")
+        if address is not None:
+            zone_location = response.css("div.location-container p::text").get()
+            section_caracteristicas = response.xpath("//ul[@id='section-caracteristicas']/li")
+            section_superficies = response.xpath("//ul[@id='section-superficie']/li")
+            section_inst_edif = response.xpath("//ul[@id='section-instalaciones-edificio']/li")
 
-        characteristicas = self.clean_sections(section_resp=section_caracteristicas)
-        superficies = self.clean_sections(section_resp=section_superficies)
-        instalaciones = [item.css("li ::text").get().replace(" ","").split("\n")[1] for item in section_inst_edif]
+            characteristics = self.clean_sections(section_resp=section_caracteristicas)
+            superficies = self.clean_sections(section_resp=section_superficies)
+            instalaciones = [item.css("li ::text").get().replace(" ","").split("\n")[1].lower() for item in section_inst_edif]
 
-class ZonapropSpider(scrapy.Spider):
-    name = "zonaprop_spider"
-    allowed_domains = ["www.zonaprop.com.ar"]
-    start_urls = ["https://www.zonaprop.com.ar/departamentos-venta-rosario.html"]
+            yield {
+            ## Add data variable here for each real estate
+                "price"             : response.css("div.titlebar p::text").get().replace(" ","").split("\n")[1],
+                "address"           : address,
+                "acceso_calle"      : self.get_street_type(address), # DUMMY
+                # "acceso_condominio" : acceso_condominio, # DUMMY
+                # "condominio"        : condominio, # DUMMY
+                "zone_location"     : zone_location,
+                "barrio"            : zone_location.split(",")[0],
+                "dormitorios"       : characteristics["Cant.Dormitorios"] if "Cant.Dormitorios" in characteristics else 0,
+                "baños"             : characteristics["Cant.Baños"] if "Cant.Baños" in characteristics else 0,
+                "cocheras"          : characteristics["Cant.Cocheras"] if "Cant.Cocheras" in characteristics else 0,
+                "superficie_total"  : float(superficies["Sup.Cubierta"].split("m2")[0]) if "Sup.Cubierta" in superficies else 0.00 + (float(superficies["Sup.Descubierta"].split("m2")[0]) if "Sup.Descubierta" in superficies else 0.00),
+                "piscina"           : 1 if "pileta" in instalaciones else 0,
+                "amenities"         : 1 if len(instalaciones)>0 and (elem in AMENITIES for elem in instalaciones) else 0
+            }
 
-    def __init__(self, *args, **kwargs):
-        super(ZonapropSpider, self).__init__(*args, **kwargs)
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        self.driver = uc.Chrome(options=chrome_options)
 
-    def parse(self, response):
-        self.driver.get(response.url)
-        selenium_response = HtmlResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
-        cards_container = selenium_response.css(".CardContainer-sc-1tt2vbg-5.fvuHxG")
-        self.logger.info(f"ACA: {cards_container}")
-        # for card in cards_container:
-        #     # price_container = card.find_element(by=By.CSS_SELECTOR, value=".PriceContainer-sc-12dh9kl-2.ePWLec")
-        #     # address_location = card.find_element(by=By.CSS_SELECTOR, value=".LocationBlock-sc-ge2uzh-1.cVCbkm").text.split("\n")
-
-        #     yield {
-        #         'url'       : card.css('a').attrib['href']
-        #         # 'address'   : address_location[0],
-        #         # 'location'  : address_location[1],
-        #         # 'price'     : price_container.find_element(by=By.CSS_SELECTOR, value=".Price-sc-12dh9kl-3.geYYII").text
-        #     }
-
-    #         # relative_estate = estate.css('a').attrib['href']
-
-    #         # if relative_estate is not None:
-    #         #     real_estate_url = 'https://www.argenprop.com' + relative_estate
-    #         #     yield response.follow(real_estate_url, callback=self.parse_real_estate_page)
-
-    #     next_page = response.css('li.pagination__page-next.pagination__page a::attr(href)').get()
-
-    #     if next_page is not None:
-    #         next_page_url = 'https://www.argenprop.com' + next_page
-    #         yield response.follow(next_page_url, callback=self.parse)
-
-    # def parse_real_estate_page(self, response):
-    #     pass
+# TO SAVE RESULT DATA EXTRACTING:
+#   scrapy crawl argenprop -O namefile.json
